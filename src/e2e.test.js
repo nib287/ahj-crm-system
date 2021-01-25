b9135fab34e532a0487a57b84512c1ea1410585f
Igor Nikolauk <nib287@yandex.ru>
2021-01-25 22:48:34 +0300

    fix: test config

--- src/e2e.test.js
+++ src/e2e.test.js
@@ -21,9 +21,9 @@ describe('CRM', () => {
     });
 
     browser = await puppetteer.launch({
-      headless: false, // show gui
-      slowMo: 250,
-      devtools: true, // show devTools
+      // headless: false, // show gui
+      // slowMo: 250,
+      // devtools: true, // show devTools
     });
     page = await browser.newPage();
   });
